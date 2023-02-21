06f19e794c1710fc1311f99819aa9532701b75d3
Tim-Vuong <72631688+Tim-Vuong@users.noreply.github.com>
2023-02-21 00:35:53 -0500

    Changed dataset to CASIA, Andrew pair programming to create triplets

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -1,16 +1,35 @@
 import pandas as pd
 import os
+import csv
 from dotenv import load_dotenv
 from PIL import Image
 
 load_dotenv()
 local_path = os.getenv('DATASET_PATH')
-print(local_path)
 
-def read_csv(file):
-    df = pd.read_csv(file)
-    # print(file.head())
-    img = Image.open("real_vs_fake/real-vs-fake/" + df['path'][0])
-    img.show()
+table_paths = []
 
-read_csv('/Users/timvuong/Desktop/hackWeek/archive/train.csv')
+tp_list = os.listdir(local_path + 'CASIA2.0_revised/Tp')
+au_list = os.listdir(local_path + 'CASIA2.0_revised/Au')
+gt_list = os.listdir(local_path + 'CASIA2.0_Groundtruth')
+
+for tp_path in tp_list:
+    parts = tp_path.replace('.', '_').split('_')[5:8]
+    paths = []
+    paths.append([s for s in au_list if f'{parts[0][:3]}_{parts[0][3:]}' in s][0])
+    paths.append([s for s in gt_list if '_'.join(parts) in s][0])
+    paths.append(tp_path)
+    
+    table_paths.append(paths)
+
+
+with open('ids.csv', 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+
+    header = ["source", "target", "tampered"]
+    # write the header
+    writer.writerow(header)
+
+    # write multiple rows
+    writer.writerows(table_paths)
+
