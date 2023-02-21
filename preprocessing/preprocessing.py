3a329dc20d33c9d5f79bbe54b405ec002c28c1a7
Tim-Vuong <72631688+Tim-Vuong@users.noreply.github.com>
2023-02-20 22:42:24 -0500

    Preprocessing files

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -1,1 +1,16 @@
-    
+import pandas as pd
+import os
+from dotenv import load_dotenv
+from PIL import Image
+
+load_dotenv()
+local_path = os.getenv('DATASET_PATH')
+print(local_path)
+
+def read_csv(file):
+    df = pd.read_csv(file)
+    # print(file.head())
+    img = Image.open("real_vs_fake/real-vs-fake/" + df['path'][0])
+    img.show()
+
+read_csv('/Users/timvuong/Desktop/hackWeek/archive/train.csv')
