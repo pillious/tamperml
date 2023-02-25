81c382d679de09a3cd91e1bff90e87ffad01b9fa
Andrew Lee <andrewzhlee@gmail.com>
2023-02-24 22:38:20 -0500

    loading overlay

--- client1/src/App.js
+++ client1/src/App.js
@@ -6,18 +6,18 @@ import ResultCard from './Components/ResultCard/ResultCard.jsx';
 function App() {
     const [files, setFiles] = useState([]);
     const [predictions, setPredictions] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     // Add this function to handle adding files from the FileUploader component
     const handleAddFile = (incomingFiles) => {
-        // setFiles((prev) => [...prev, ...incomingFiles]);
-        console.log(incomingFiles);
         setFiles(incomingFiles);
     };
 
     const postImage = () => {
         const filesBase64 = files.map((f) => f.getFileEncodeBase64String());
-        console.log(filesBase64);
 
+        setIsLoading(true);
+        document.body.style.overflow = 'hidden';
         fetch('http://localhost:5000/analyze', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -25,16 +25,23 @@ function App() {
         })
             .then((res) => res.json())
             .then((res) => {
-                console.log(files);
-                console.log(res.data);
                 setPredictions(res.data);
+                setIsLoading(false);
+                document.body.style.overflow = 'unset';
             })
-            .catch((err) => console.log(err));
+            .catch((err) => {
+                console.log(err);
+                setIsLoading(false);
+                document.body.style.overflow = 'unset';
+            });
     };
 
     return (
         <main>
             <div className='App'>
+                <div className={`overlay ${isLoading === true ? '' : 'hide'}`}>
+                    <div className='spinner' />
+                </div>
                 <header className='App-header'>
                     <h1 id='siteName'>TamperML</h1>
                     <div className='uploadArea'>
--- client1/src/Components/DragAndDrop.jsx
+++ client1/src/Components/DragAndDrop.jsx
@@ -1,55 +1,46 @@
-import React, { useState } from "react";
-import AnalyzeButton from "./AnalyzeButton.jsx";
-import FileUploader from "./FileUploader";
+import AnalyzeButton from './AnalyzeButton.jsx';
+import FileUploader from './FileUploader';
 
 // Import React FilePond
-import { FilePond, registerPlugin } from "react-filepond";
+import { FilePond, registerPlugin } from 'react-filepond';
 
 // Import FilePond styles
-import "filepond/dist/filepond.min.css";
+import 'filepond/dist/filepond.min.css';
 
 // Import the Image EXIF Orientation and Image Preview plugins
 // Note: These need to be installed separately
 // `npm i filepond-plugin-image-preview filepond-plugin-image-exif-orientation --save`
 import FilePondPluginFileEncode from 'filepond-plugin-file-encode';
-import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
-import FilePondPluginImagePreview from "filepond-plugin-image-preview";
-import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
+import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation';
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
 
 // Register the plugins
-registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview, FilePondPluginFileEncode);
+registerPlugin(
+    FilePondPluginImageExifOrientation,
+    FilePondPluginImagePreview,
+    FilePondPluginFileEncode
+);
 
-// Our app
 const DragAndDrop = (props) => {
-  // const [files, setFiles] = useState([]);
+    return (
+        <div>
+            <FilePond
+                files={props.files}
+                onupdatefiles={props.handleAddFile}
+                allowMultiple={true}
+                maxFiles={20}
+                name='files'
+                labelIdle='Drag & Drop your files* or <span class="filepond--label-action">Browse</span>'
+            />
 
-  // // Add this function to handle adding files from the FileUploader component
-  // const handleAddFile = (file) => {
-  //   setFiles([...files, file]);
-  // };
-
-  return (
-    <div>
-      <FilePond
-        files={props.files}
-        onupdatefiles={props.handleAddFile}
-        allowMultiple={true}
-        maxFiles={20}
-        // server={{
-        //     url: "http://localhost:5000/upload",
-        //     headers: {'Content-Type': 'application/json'}
-        // }}
-        name="files"
-        labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
-      />
-    
-        <div className="buttons">
-          <FileUploader handleFile={props.handleAddFile} />            
-          <AnalyzeButton files={props.files} postImage={props.postImage}/>
+            <div className='disclaimer'>*Supports .jpg, .jpeg, and .png files.</div>
+            <div className='buttons'>
+                <FileUploader handleFile={props.handleAddFile} />
+                <AnalyzeButton files={props.files} postImage={props.postImage} />
+            </div>
         </div>
-    </div>
-  );
+    );
 };
-
 
 export default DragAndDrop;
